a734aedbdaef188661ac99a95d64324fb1978d15
SOryx1 <alan.burt@mackillop.act.edu.au>
2016-11-02 13:30:09 +1100

    Initial Template
    This will be the base we are working.
    this file includes sensors which we will be using nothing more.
    a few comments

---
+++ Burtle.java
@@ -0,0 +1,42 @@
+import lejos.nxt.*;
+import lejos.util.*;
+
+/**
+The Burtle program and robot will attempt to find a light source
+through a maze.
+
+Author Seth East
+
+*/
+
+public class Burtle {
+	
+	//sensors created
+	
+	UltrasonicSensor ultra;
+
+	private LightSensor light;
+	
+	private final int SETPOINT = 50;
+	
+	public Burtle () {
+		
+		//sensors 
+		
+		ultra = new UltrasonicSensor(SensorPort.S1);
+		
+		light = new LightSensor(SensorPort.S4);
+		light.setFloodlight(false);
+		
+		run();
+	}
+	
+	private void run(){
+		Delay.msDelay(1000);
+		
+	}
+	
+	public static void main (String[] args) {
+	new Burtle();
+	}
+}
